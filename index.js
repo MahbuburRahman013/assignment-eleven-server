1d9e17ff478345f59f15f0f05b869e05f6bbc95f
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-11-06 10:19:43 +0600

    added delete assignment oparetion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -43,6 +43,14 @@ async function run() {
         }
         const result = await assignmentCollection.find(query).toArray()
         res.send(result)
+    })
+
+
+    app.delete('/delete-assignment/:id', async(req, res) => {
+         const id = req.params.id;
+         const query = {_id: new ObjectId(id)};
+         const result = await assignmentCollection.deleteOne(query);
+         res.send(result)
     })
 
     // Send a ping to confirm a successful connection
