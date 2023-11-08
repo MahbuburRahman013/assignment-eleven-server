234beefad334f36e611707d4482f78b09e327418
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-11-08 19:26:09 +0600

    deployed

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:[
+    
+    'https://assignment-eleven-66826.web.app',
+    'https://assignment-eleven-66826.firebaseapp.com'
+
+],
   credentials:true
 }));
 app.use(express.json());
@@ -50,7 +55,7 @@ const verifyToken = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
          
     const assignmentCollection = client.db('assignmentEleven').collection('assignment');
     const submitAssignmentCollection = client.db('assignmentEleven').collection('submitAssignment');
@@ -198,10 +203,10 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
